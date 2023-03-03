936de54f0077afdab86caf39bea9fd782d080870
Dixen-Naxos <armanddailly@gmail.com>
2023-03-03 10:34:05 +0100

    api url

--- lib/service/service_api.dart
+++ lib/service/service_api.dart
@@ -9,16 +9,16 @@ class ServiceAPI {
   final dio = Dio();
 
   Future<Distributor> getDistributor() async {
-    final response = await dio.get('http://34.105.224.254/distributors/477b0e36-e58e-4596-a31f-a6801fcd2c31');
+    final response = await dio.get('http://146.59.237.29:7790/distributors/477b0e36-e58e-4596-a31f-a6801fcd2c31');
     return Distributor.fromJson(jsonDecode(response.toString()));
   }
 
   Future<void> buyProduct(String user_id, String product_id) async {
-    await dio.put('http://34.105.224.254/distributors/477b0e36-e58e-4596-a31f-a6801fcd2c31/buyProduct/' + product_id + '/' + user_id);
+    await dio.put('http://146.59.237.29:7790/distributors/477b0e36-e58e-4596-a31f-a6801fcd2c31/buyProduct/' + product_id + '/' + user_id);
   }
 
   Future<User> getUser(String user_id) async {
-    final response = await dio.get("http://34.105.224.254/users/" + user_id);
+    final response = await dio.get("http://146.59.237.29:7790/users/" + user_id);
     return User.fromJson(jsonDecode(response.toString()));
   }
 }
